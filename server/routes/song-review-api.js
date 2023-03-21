579d82e9808d03747cc0199b7a75852af0360c67
narmeen <narmeen.kh99@gmail.com>
2023-03-21 14:35:14 +0200

    create postReq

--- server/routes/song-review-api.js
+++ server/routes/song-review-api.js
@@ -1,12 +1,49 @@
-const express = require('express')
-const router = express.Router()
-const SongReview = require('../models/SongReview')
+const express = require("express");
+const router = express.Router();
+const SongReview = require("../models/SongReview");
 
-router.get('/songs-reviews', function(req, res) {
-    SongReview.find({})
-        .then(function(SongsReviews) {
-            res.send(SongsReviews)
-        }).catch((err) => res.status(500).send({ msg: "Internal server error" }))
-})
+router.get("/songs-reviews", function (req, res) {
+  SongReview.find({})
+    .then(function (SongsReviews) {
+      res.send(SongsReviews);
+    })
+    .catch((err) => res.status(500).send({ msg: "Internal server error" }));
+});
+router.post("/songs-reviews", function (req, res) {
+  const newPost = new post({
+    title: req.body.title,
+    artist: req.body.artist,
+    source: req.body.source,
+    user: req.body.user,
+    date_created: req.body.date_created,
+    category: req.body.category,
+    reactions: {
+      likes: req.body.reactions[0],
+      dislikes: req.body.reactions[1],
+      wow: req.body.reactions[2],
+      angry: req.body.reactions[3],
+    },
+    review: req.body.review,
+  });
+  console.log(newPost);
+  newPost.save();
+  res.send(`the post that created by  ${newPost.createdBy} is saved`);
+});
 
-module.exports = router
+/*router.get("/songs-reviews/:word", (req, res) => {
+    let Gluten = req.query.gluten;
+   // let Dairy = req.query.dairy;
+    let word = req.params.word;
+    arr = [];
+    if (word != "") {
+      arr.push(word);
+    }
+    axios
+      .get(
+        ``
+      )
+      .then((SongReview) => {
+        res.send(songsFilter(SongReview, arr));
+      });
+  });
+module.exports = router;*/
