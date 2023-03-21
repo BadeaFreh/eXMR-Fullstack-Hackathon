752eef72bb4352f6155dd54bb43af79ad0a962ca
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-03-21 18:54:25 +0200

    script tag for handlebars

--- public/views/home-renderer.js
+++ public/views/home-renderer.js
@@ -1,10 +1,9 @@
 class HomeRender{
     renderReviews(reviewsArray) {
-        
         $('#reviews-section').empty()
         const source = $('#reviews-template').html();
         const template = Handlebars.compile(source);
         const newHTML = template({ review: reviewsArray });
-        $('#reviews-section' ).append(newHTML);
+        $('#reviews-section').append(newHTML);
      }
 }
