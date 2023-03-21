dca42cfc9d206e2a1e4587ce0ccbed7599b32dfb
narmeen <narmeen.kh99@gmail.com>
2023-03-21 17:38:56 +0200

    filtering songs reviews by category

--- server/routes/song-review-api.js
+++ server/routes/song-review-api.js
@@ -2,19 +2,31 @@ const express = require("express");
 const router = express.Router();
 const SongReview = require("../models/SongReview");
 
+songsFilter = function (SongsReviews, filter) {
+  let newArrSongs = [];
+  for (song of SongsReviews) {
+    if (filter == song._doc.category) {
+      newArrSongs.push(song);
+    }
+  }
+  return [...newArrSongs];
+};
+
 router.get("/songs-reviews", function (req, res) {
+  let category = req.query.category;
   SongReview.find({})
-    .then(function (SongsReviews) {
-      res.send(SongsReviews);
+    .then((SongReview) => {
+      res.send(songsFilter(SongReview, category));
     })
     .catch((err) => res.status(500).send({ msg: "Internal server error" }));
 });
+
 router.post("/songs-reviews", function (req, res) {
   const newPost = new SongReview({
     title: req.body.title,
     artist: req.body.artist,
     source: req.body.source,
-    user: req.body.user,
+    //user: req.body.user,
     date_created: req.body.date_created,
     category: req.body.category,
     reactions: {
@@ -27,7 +39,7 @@ router.post("/songs-reviews", function (req, res) {
   });
   console.log(newPost);
   newPost.save();
-  res.send(`the post that created by  ${newPost.createdBy} is saved`);
+  res.send(`the post that created by  ${newPost.user} is saved`);
 });
 router.delete("/post/:postID", function (req, res) {
   SongReview.findOneAndRemove({ _id: req.params.postID }).then(function (
