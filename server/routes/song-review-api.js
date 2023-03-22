f3986d0ba0ffdecdc9177df6e3d3ca69cade66e5
Yahya Ameen <yahyaameen33@gmail.com>
2023-03-22 02:04:48 +0200

    sorting and filtering features in backEnd

--- server/routes/song-review-api.js
+++ server/routes/song-review-api.js
@@ -1,44 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const SongReview = require("../models/SongReview");
-
+const User = require("../models/User");
 const songsReviewsUtils = require("../utils/songs-reviews-utils");
 
 router.get("/songs-reviews", function(req, res) {
-    let category = req.query.category;
-    SongReview.find({}).populate("user")
+    let category = req.query.category
+    let reactionType = req.query.reactionType
+
+    songsReviewsUtils.relevantSongsReviews(category, reactionType)
         .then((SongsReviews) => {
-            res.send(songsReviewsUtils.songsFilterByCategory(SongsReviews, category));
+            const relevantSongsReviewsArray = songsReviewsUtils.songsReviewsfiltering(SongsReviews)
+            res.send(relevantSongsReviewsArray)
         })
-        .catch((err) => res.status(500).send({ msg: "Internal server error" }));
+        .catch((err) => res.status(500).send({ msg: "Internal server error" }))
+
 });
 
-router.post("/songs-reviews", function(req, res) {
-    const newPost = new SongReview({
-        title: req.body.title,
-        artist: req.body.artist,
-        source: req.body.source,
-        //user: req.body.user,
-        date_created: req.body.date_created,
-        category: req.body.category,
-        reactions: {
-            likes: req.body.reactions[0],
-            dislikes: req.body.reactions[1],
-            wow: req.body.reactions[2],
-            angry: req.body.reactions[3],
-        },
-        review: req.body.review,
-    });
-    console.log(newPost);
-    newPost.save();
-    res.send(`the post that created by  ${newPost.user} is saved`);
+router.post("/song-review", function(req, res) {
+    const songReview = new SongReview(req.body)
+    songReview.save()
+    res.status(201).send({ "message": "song review created" })
 });
-router.delete("/post/:postID", function(req, res) {
-    SongReview.findOneAndRemove({ _id: req.params.postID }).then(function(
-        result
-    ) {
-        res.send(`the post deleted`);
-    });
-});
+router.delete("/song-review/:songReviewID", function(req, res) {
+    SongReview.findOneAndRemove({ _id: req.params.songReviewID })
+        .then(function(result) {
+            res.status(204).send({ "message": "song review removed" });
+        });
+})
+
+router.post("/userSignUp", function(req, res) {
+    const user = new User(req.body)
+    user.save()
+    res.status(201).send({ "message": "user created" })
+})
+
+router.post("/userSignIn", function(req, res) {
+    const user = new User(req.body)
+        //to do
+})
 
 module.exports = router;
--- server/utils/songs-reviews-utils.js
+++ server/utils/songs-reviews-utils.js
@@ -1,10 +1,50 @@
-songsFilterByCategory = function(SongsReviews, songCategory) {
-    let songsReviewsByCategory = [];
-    for (const songReview of SongsReviews) {
-        if (songCategory === song._doc.category) {
-            songsReviewsByCategory.push(songReview);
-        }
+const SongReview = require("../models/SongReview");
+
+const relevantSongsReviews = function(category, reactionType) {
+    if (category && reactionType) {
+        return songsReviewsSortingAndFiltering(category, reactionType)
+    } else if (category) {
+        return songsReviewsFiltering(category)
+    } else if (reactionType) {
+        return songsReviewsSorting(reactionType)
+    } else {
+        return songsReviews()
     }
-    return songsReviewsByCategory;
-};
-module.exports = { songsFilterByCategory };
+}
+
+const songsReviewsfiltering = function(SongsReviews) {
+    let songsReviewsfiltering = []
+    for (const songReview of SongsReviews) {
+        let songReviewfilter = {}
+        songReviewfilter._id = songReview._id
+        songReviewfilter.songTitle = songReview.songTitle
+        songReviewfilter.singer = songReview.singer
+        songReviewfilter.source = songReview.source
+        songReviewfilter.user = songReview.user.name
+        songReviewfilter.datePosted = songReview.datePosted
+        songReviewfilter.genre = songReview.genre
+        songReviewfilter.reactions = songReview.reactions
+        songReviewfilter.review = songReview.review
+
+        songsReviewsfiltering.push(songReviewfilter)
+    }
+    return songsReviewsfiltering
+}
+
+const songsReviewsSortingAndFiltering = function(category, reactionType) {
+    return SongReview.find({ category: category }).populate("user").sort({ "reactions.reactionType": -1 })
+}
+
+const songsReviewsFiltering = function(category) {
+    return SongReview.find({ category: category }).populate("user")
+}
+
+const songsReviewsSorting = function(reactionType) {
+    return SongReview.find({}).populate("user").sort({ "reactions.reactionType": -1 })
+}
+
+const songsReviews = function() {
+    return SongReview.find({}).populate("user")
+}
+
+module.exports = { relevantSongsReviews, songsReviewsfiltering };
