1e3cd2f3380e247f233bb3c166b168307c57e932
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-03-22 09:53:37 +0200

    css changes

--- public/views/home-renderer.js
+++ public/views/home-renderer.js
@@ -1,18 +1,9 @@
  class HomeRender{
     renderReviews(reviewsArray) {
-<<<<<<< HEAD
-        
-        //$('#reviews-section').empty()
-=======
         $('#reviews-section').empty()
->>>>>>> master
         const source = $('#reviews-template').html();
         const template = Handlebars.compile(source);
         const newHTML = template({ review: reviewsArray });
         $('#reviews-section').append(newHTML);
      }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> master
