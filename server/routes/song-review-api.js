16a6bf5942092ca9b4ad3bf37d15b98f10b696eb
BadeaFreh <71319309+BadeaFreh@users.noreply.github.com>
2023-05-04 17:48:07 +0300

    fixed error

--- server/routes/song-review-api.js
+++ server/routes/song-review-api.js
@@ -1,42 +1,45 @@
-const express = require("express");
-const router = express.Router();
-const SongReview = require("../models/SongReview");
-const User = require("../models/User");
-const songsReviewsUtils = require("../utils/songs-reviews-utils");
+const express = require('express')
+const router = express.Router()
+const SongReview = require('../models/SongReview')
+const User = require('../models/User')
+const songsReviewsUtils = require('../utils/songs-reviews-utils')
 // const songsReviewsData = require('../data/dummy-data')
-const {ObjectId} = require('bson')
+const { ObjectId } = require('bson')
 
-router.get("/songs-reviews", function(req, res) {
-    let genre = req.query.genre
+router.get('/songs-reviews', function (req, res) {
+    let category = req.query.category
     let reactionType = req.query.reactionType
     // songsReviewsUtils.insertData(songsReviewsData)
-    songsReviewsUtils.relevantSongsReviews(category, reactionType)
-    .then((SongsReviews) => {
+    songsReviewsUtils
+        .relevantSongsReviews(category, reactionType)
+        .then((SongsReviews) => {
             res.send(SongsReviews)
         })
-        .catch((err) => res.status(500).send({ msg: "Internal server error" }))
+        .catch((err) => res.status(500).send({ msg: 'Internal server error' }))
 })
 
-router.post("/song-review", function(req, res) {
+router.post('/song-review', function (req, res) {
     const songReview = new SongReview(req.body)
     songReview.save()
-    res.status(201).send({ "message": "song review created" })
-});
+    res.status(201).send({ message: 'song review created' })
+})
 
-router.put('/song-review/:songReviewID', function(req, res) {
+router.put('/song-review/:songReviewID', function (req, res) {
     let songReviewID = req.params.songReviewID
     const updatedSongReview = req.body
-    SongReview.findByIdAndUpdate(songReviewID, updatedSongReview)
-        .then(function(result) {
-            res.send({ "message": "song review updated" });
-        });
+    SongReview.findByIdAndUpdate(songReviewID, updatedSongReview).then(
+        function (result) {
+            res.send({ message: 'song review updated' })
+        }
+    )
 })
 
-router.delete("/song-review/:songReviewID", function(req, res) {
-    SongReview.findOneAndRemove({ _id: req.params.songReviewID })
-        .then(function(result) {
-            res.status(204).send({ "message": "song review removed" });
-        });
+router.delete('/song-review/:songReviewID', function (req, res) {
+    SongReview.findOneAndRemove({ _id: req.params.songReviewID }).then(
+        function (result) {
+            res.status(204).send({ message: 'song review removed' })
+        }
+    )
 })
 
-module.exports = router;
+module.exports = router
